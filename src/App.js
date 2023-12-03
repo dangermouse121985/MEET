f9b6c35793f6c0d1b126a5951b9458dbe31c5344
David <618375+dangermouse121985@users.noreply.github.com>
2023-12-03 15:32:23 -0500

    Created Initial Test scripts

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 import EventList from './components/EventList';
+import CitySearch from './components/CitySearch';
 
 const App = () => {
   return (
     <div className="App">
+      <CitySearch />
       <EventList />
     </div>
   );
