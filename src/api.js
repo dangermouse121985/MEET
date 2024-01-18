5fb0acc7c52f6997a735c3a0c3f7d455a4a7bb0b
David <618375+dangermouse121985@users.noreply.github.com>
2024-01-17 21:34:07 -0500

    test change to url string

--- src/api.js
+++ src/api.js
@@ -33,7 +33,7 @@ export const getEvents = async () => {
 
     if (token) {
         removeQuery();
-        const url = "https://2dqc5ocp1i.execute-api.us-east-1.amazonaws.com/dev/api/get-events" + "/" + token;
+        const url = 'https://2dqc5ocp1i.execute-api.us-east-1.amazonaws.com/dev/api/get-events/' + token;
         const response = await fetch(url);
         const result = await response.json();
         if (result) {
@@ -63,7 +63,7 @@ const removeQuery = () => {
 const getToken = async (code) => {
     const encodeCode = encodeURIComponent(code);
     const response = await fetch(
-        'https://2dqc5ocp1i.execute-api.us-east-1.amazonaws.com/dev/api/token' + '/' + encodeCode
+        'https://2dqc5ocp1i.execute-api.us-east-1.amazonaws.com/dev/api/token/' + encodeCode
     );
     const { access_token } = await response.json();
     access_token && localStorage.setItem("access_token", access_token);
