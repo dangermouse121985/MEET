790b0c0f7599229cbf27f9aceebcc6507dfdf800
David <618375+dangermouse121985@users.noreply.github.com>
2024-02-15 20:09:43 -0500

    Corrected the margins on the chart

--- src/components/CityEventsChart.js
+++ src/components/CityEventsChart.js
@@ -11,20 +11,20 @@ const CityEventsChart = ({ allLocations, events }) => {
     const getData = () => {
         const data = allLocations.map((location) => {
             const count = events.filter((event) => event.location === location).length;
-            const city = location.split(', ')[0];
+            const city = location.split((/, | - /))[0];
             return { city, count };
         })
         return data;
     };
 
     return (
-        <ResponsiveContainer width="99%" height={400}>
+        <ResponsiveContainer width="90%" height={450}>
             <ScatterChart
                 margin={{
                     top: 20,
                     right: 20,
-                    bottom: 20,
-                    left: 20,
+                    bottom: 100,
+                    left: -30,
                 }}
             >
                 <CartesianGrid />
